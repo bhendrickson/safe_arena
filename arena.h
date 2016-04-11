4a061cd451e2ec8ad244be48bcbba15dcfdb12c8
Ben Hendrickson <ben@1m7.com>
2016-04-11 11:03:13 -0700

    Moving member functions to be inline

--- arena.h
+++ arena.h
@@ -11,14 +11,13 @@ namespace abc {
 
 class arena {
  public:
-  template <class T>
-  gsl::span<T> allocate_span(std::ptrdiff_t n) {
+  template<class T> gsl::span<T> allocate_span(std::ptrdiff_t count) {
     static_assert(std::is_trivially_destructible<T>::value,
                   "can only allocate trivially destructible types");
     static_assert(alignof(T) <= alignof(std::max_align_t),
                   "can only allocate types with a fundamental alignment");
 
-    size_t size = sizeof(T) * n;
+    size_t size = sizeof(T) * count;
     uintptr_t padding = alignment_padding<alignof(T)>(ptr_);
     size_t padded_size = size + padding;
 
@@ -31,11 +30,10 @@ class arena {
       space_ -= padded_size;
     }
 
-    return gsl::span<T>(new(space_to_use) T[n], n);
+    return gsl::span<T>(new(space_to_use) T[count], count);
   }
 
-  template <class T>
-  T* allocate() {
+  template<class T> T* allocate() {
     return allocate_span<T>(1).data();
   }
 
@@ -44,33 +42,30 @@ class arena {
   size_t space_ = 0;
   char* ptr_ = nullptr;
 
-  template<uintptr_t Align>
-  static uintptr_t alignment_padding(char* p) {
+  template<uintptr_t I> static uintptr_t alignment_padding(char* p) {
     auto i = reinterpret_cast<uintptr_t>(p);
-    return (i + Align - 1) & ~(Align - 1) - i;
+    return (i + I - 1) & ~(I - 1) - i;
   }
 
-  char* fallback_allocate(size_t size);
+  char* fallback_allocate(size_t size) {
+    std::unique_ptr<char[]> new_block;
+
+    static const size_t kBlockSize = 4096;
+    if (size < kBlockSize / 4) {
+      new_block = std::make_unique<char[]>(kBlockSize);
+      space_ = kBlockSize - size;
+      ptr_ = new_block.get() + size;
+    } else {
+      // Allocate large items by themselves to waste less at ends of blocks.
+      new_block = std::make_unique<char[]>(size);
+    }
+
+    // Alignment from new char[]'s is sufficent for all fundamental alignments.
+    char* ret = new_block.get();
+    blocks_.push_back(std::move(new_block));
+    return ret;
+  }
 };
-
-char* arena::fallback_allocate(size_t size) {
-  std::unique_ptr<char[]> new_block;
-
-  static const size_t kBlockSize = 4096;
-  if (size < kBlockSize / 4) {
-    new_block = std::make_unique<char[]>(kBlockSize);
-    space_ = kBlockSize - size;
-    ptr_ = new_block.get() + size;
-  } else {
-    // Allocate large items by themselves to waste less at ends of blocks.
-    new_block = std::make_unique<char[]>(size);
-  }
-
-  // Alignment from new char[]'s is sufficent for all fundamental alignments.
-  char* ret = new_block.get();
-  blocks_.push_back(std::move(new_block));
-  return ret;
-}
 
 }  // namespace abc
 
